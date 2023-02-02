d81b90b90bd558e2f9e8afb4045cd7e619375ff6
Jos Warmer <jos.warmer@openmodeling.nl>
2023-02-02 13:09:28 +0100

    rename projectit to freon

--- src/webapp/config/WebappConfiguration.ts
+++ src/webapp/config/WebappConfiguration.ts
@@ -1,13 +1,14 @@
-import type { PiEnvironment } from "@projectit/core";
+import type { FreEnvironment } from "@freon4dsl/core";
 import { LanguageInitializer } from "../language/LanguageInitializer";
 import type { IServerCommunication } from "../server/IServerCommunication";
 import { ServerCommunication } from "../server/ServerCommunication";
+
 
 /**
  * The one and only reference to the actual language for which this editor runs
  */
 import { MyLanguageEnvironment } from "../../picode/config/gen/MyLanguageEnvironment";
-export const editorEnvironment: PiEnvironment = MyLanguageEnvironment.getInstance();
+export const editorEnvironment: FreEnvironment = MyLanguageEnvironment.getInstance();
 LanguageInitializer.initialize();
 
 /**
